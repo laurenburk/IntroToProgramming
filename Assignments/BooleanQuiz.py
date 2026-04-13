1692e555d72a3254b4ab46b6b44e13d7b6d3575a
laurenburk <burklauren9@gmail.com>
2026-04-13 14:17:05 -0500

    boolean quiz

---
+++ Assignments/BooleanQuiz.py
@@ -0,0 +1,14 @@
+answer_one = int(input("Give a whole number less than or equal to 7\n->"))
+print(answer_one <= 7)
+
+answer_two = int(input("Give a whole number greater than 10\n->"))
+print(answer_two > 10)
+
+answer_three = int(input("Give a whole number that is negative\n->"))
+print(answer_three < 0)
+
+answer_four = int(input("Give a number less than 50\n->"))
+print(answer_four < 50)
+
+answer_five = int(input("Give a number greater than or equal to 1,000\n->"))
+print(answer_five >= 1000)
