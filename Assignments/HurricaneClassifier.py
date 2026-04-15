6fcb319beed87aad39e4af4c1d2780b330061c75
laurenburk <burklauren9@gmail.com>
2026-04-15 14:29:49 -0500

    hurricane

--- Assignments/HurricaneClassifier.py
+++ Assignments/HurricaneClassifier.py
@@ -1,19 +1,23 @@
 wind_speed = int(input("Please enter the wind speed of the hurricane in miles per hour:\n->"))
 
-if (wind_speed >= 157):
-    print("That is a Category 5 Hurricane")
+def hurricane_category(wind_speed):
+    try:
+        if (wind_speed >= 157):
+            return "That is a Category 5 Hurricane."
 
-elif (wind_speed < 157 and wind_speed >= 130):
-    print("That is a Category 4 Hurricane.")
+        elif (wind_speed < 157 and wind_speed >= 130):
+            return "That is a Category 4 Hurricane."
           
-elif (wind_speed < 130 and wind_speed >= 111):
-    print("That is a Category 3 Hurricane.")
+        elif (wind_speed < 130 and wind_speed >= 111):
+            return "That is a Category 3 Hurricane."
 
-elif (wind_speed < 111 and wind_speed >= 96):
-    print("That is a Category 2 Hurricane.")
+        elif (wind_speed < 111 and wind_speed >= 96):
+            return "That is a Category 2 Hurricane."
 
-elif (wind_speed < 96 and wind_speed >= 74):
-    print("That is a Category 1 Hurricane.")
+        elif (wind_speed < 96 and wind_speed >= 74):
+            return "That is a Category 1 Hurricane."
+    
+    except (wind_speed < 74):
+        print("You are overreacting. That is not a Hurricane.")
 
-else:
-    print("That is more of a Tropical Storm than a Hurricane.")
+print(hurricane_category(wind_speed))
