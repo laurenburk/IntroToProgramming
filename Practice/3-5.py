3ac98f1cc65b32b0f2cd455ad11ed235a7244848
laurenburk <burklauren9@gmail.com>
2026-04-16 15:18:17 -0500

    3-5

--- Practice/3-5.py
+++ Practice/3-5.py
@@ -15,3 +15,12 @@ if age >= 18:
 
 else:
     print("Access Denied: Age Restriction")
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    
+    else:
+        return "Odd"
+    
+even_or_odd()
