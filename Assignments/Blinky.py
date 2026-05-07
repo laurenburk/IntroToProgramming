7f2d8d57b32fbf208ee08fd6e2f201a586a3e22a
laurenburk <burklauren9@gmail.com>
2026-05-07 15:15:31 -0500

    Blinky and woosh

--- Assignments/Blinky.py
+++ Assignments/Blinky.py
@@ -2,11 +2,10 @@ from adafruit_circuitplayground import cp
 
 import time
 
-cp.pixels.brightness = 0.1
+cp.pixels.brightness = 0.05
 
 while True:
-    if cp.button_a:
-        for i in range(0,10):
-            cp.pixels[i] = (0,0,225)
-            time.sleep(.1)
-            cp.pixels[i] = (0,0,0)
+    cp.pixels.fill((0,225,0))
+    time.sleep(.367)
+    cp.pixels.fill((0,0,0))
+    time.sleep(.367)
